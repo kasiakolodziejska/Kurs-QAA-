b89aff274194fb5bc8409667155b2ab605b49adf
Kasia <120297090+kasiakolodziejska@users.noreply.github.com>
2023-12-14 11:38:17 +0100

    add new js file

---
+++ Zajęcia 3 - javascript/main2.js
@@ -0,0 +1,40 @@
+"use strict"
+
+console.log("------QA-----")
+const user = "Adam"
+console.log('your username is ${user}"')
+console.log(`your username is ${user}`)
+console.log("your username is " + user)
+const message = `your username is ${user}`
+// const message2 = "your username is", user
+console.log("your username is", user)
+
+console.log("----Operatory logiczne-----")
+
+
+// falsy values
+// 0
+// NaN
+// null
+// undefined
+// ''
+// false
+
+// truthy values
+// 1
+// "0"
+// "false"
+// 49382
+// "fdasfas"
+// []
+// {}
+// methods
+// functions
+
+// AND
+// OR
+// Negation
+
+const age = 20
+console.log(age > 10)
+
