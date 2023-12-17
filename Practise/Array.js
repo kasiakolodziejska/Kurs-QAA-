424c2194bf4e2e5b7e32014e5ca4554ec26f99b4
Kasia <120297090+kasiakolodziejska@users.noreply.github.com>
2023-12-17 17:23:38 +0100

    adding practise materials

---
+++ Practise/Array.js
@@ -0,0 +1,29 @@
+{const clients = ["Kasia", "Zuma", "Miśka"];
+
+console.log(clients[1])
+console.log(clients[0])
+console.log(clients[2])
+
+console.log(clients)
+
+console.log(clients.length)
+
+const lastElementIndex = clients.length -1;
+console.log(lastElementIndex);
+
+console.log(clients[lastElementIndex])
+
+}
+
+console.log("-------iteracje----------")
+
+const clients = ["Kasia", "Zuma", "Ida"];
+for (let i=0; i < clients.length; i += 1) {
+    console.log(clients[1])
+}
+
+const client = ["Kasia", "Zuma", "Ida"];
+for (const client of clients) {
+    console.log(client)
+}
+
